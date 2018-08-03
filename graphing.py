93107ce0765b9a2f0e67196603148410c741cd2d
sriharis123 <sri.sub000@gmail.com>
2018-08-03 19:43:30 -0400

    made peak radius an argument with default value 200 data points [20 nm]

--- graphing.py
+++ graphing.py
@@ -34,15 +34,15 @@ def draw_savitzky_golay(x, y):
     plt.plot(x, yprime, marker='v', markevery=maxs)
     plt.show()
 
-def find_local_maxs(x, y):
+def find_local_maxs(x, y, pradius=200):
     maxs = np.array([0]) 
     # peak_radius and skip assume the shape of the data. though they're not terrible assumptions,
     #       in this case we can assume something about the underlying physics.
     # since you're looking for distance btwn A and B exciton peaks, you can assume that the peaks
     #       will be within some range of their known positions.
     # maybe start from there, instead of assuming that the peaks are >20 nm apart
-    peak_radius = 200       # checks if this is the greatest value in 200 data points [~20 nanometers]
-    r = 0                   # why aren't you starting at 0?
+    peak_radius = pradius       # checks if this is the greatest value in 200 data points [~20 nanometers]
+    r = 0
     while r < len(x):
         if y[r] < y[r - peak_radius] and y[r - peak_radius] > y[r - peak_radius * 2]:
             # (r - peak_radius) will be a negative number while r<200. [Doesn't give runtime error??]
