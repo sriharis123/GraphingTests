1a6df4d59cf80d1cadab9605b602e83dfd277dc3
sriharis123 <sri.sub000@gmail.com>
2018-08-13 00:53:32 -0400

    cleanup and delete unused arguments to plot_annotate()

--- graphing.py
+++ graphing.py
@@ -11,7 +11,7 @@ def draw_normal_plot(x, y):
     :param y: List of y-axis points
     :return: N/A
     """
-    plot_annotate(x, y, 'o', np.ndarray.tolist(find_local_maxes(x, y)), 'LMaxNorm')
+    plot_annotate(x, y, 0, np.ndarray.tolist(find_local_maxes(x, y)), plot_name='LMaxNorm')
 
 
 def draw_savitzky_golay(x, y):
@@ -22,28 +22,25 @@ def draw_savitzky_golay(x, y):
     :return: N/A
     """
     yprime = savitzky_golay(y, 31, 10, 0)
-    figure.suptitle('Subplotted Graph')
-    plot_annotate(x, yprime, plot, 0, np.ndarray.tolist(find_local_maxes(x, yprime)), plot_name='LMaxSG', color='r')
-    plot_annotate(x, yprime, plot, 1, np.ndarray.tolist(find_local_maxes_ws2(x, yprime)), plot_name='LMaxSG_WS2', color='r')
+    plot_annotate(x, yprime, 1, np.ndarray.tolist(find_local_maxes(x, yprime)), plot_name='LMaxSG', color='r')
+    plot_annotate(x, yprime, 2, np.ndarray.tolist(find_local_maxes_ws2(x, yprime)), plot_name='LMaxSG_WS2', color='r')
 
 
-def plot_annotate(x, y, plot, graph_location, maxes, marker='v', plot_name='Unnamed', color='b'):
+def plot_annotate(x, y, graph_location, maxes, plot_name='Unnamed', color='b'):
     """
     Plot with a certain color and markers, then annotates
     :param x: List of x-axis points
     :param y: List of y-axis points
-    :param plot:
     :param graph_location:
     :param maxes: List of max values in the plot (x, y)
-    :param marker:
     :param plot_name:
     :param color: Color to use when plotting
     :return: N/A
     """
-    plot[graph_location].plot(x, y, marker=marker, markevery=maxes, color=color)
-    for r in range(0, 3):
-        plt.annotate(plot_name + ': (' + (str(x[maxes[r]]) + ', ' + str(y[maxes[r]])) + ')',
-                     xy=(x[maxes[r]], y[maxes[r]]))
+    plot[graph_location].plot(x, y, marker='v', markevery=maxes, color=color)
+    for r in range(0, len(maxes)):
+        plot[graph_location].annotate(plot_name + ': (' + (str(x[maxes[r]]) + ', ' + str(y[maxes[r]])) + ')',
+                                      xy=(x[maxes[r]], y[maxes[r]]))
 
 
 def find_local_maxes(x, y, pradius=200):
@@ -270,6 +267,7 @@ if __name__ == '__main__':
         print(str(wavelengths[i]) + '\t\t' + str(intensities[i]))
     '''
     figure, plot = plt.subplots(3, sharex=True)
+    figure.suptitle('Subplotted Graph')
 
     print(find_exciton_peak_distance_ws2(wavelengths, intensities))
     draw_normal_plot(wavelengths, intensities)
